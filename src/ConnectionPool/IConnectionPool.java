8e973a0b311f579b2d97d03e564ce24399d1dff6
EdwardHome <guoran198907@sina.com>
2017-06-04 23:12:12 +0800

    Add ConnectionPool Package

---
+++ src/ConnectionPool/IConnectionPool.java
@@ -0,0 +1,28 @@
+package ConnectionPool;
+
+/**
+ * Created by Edward on 2017/6/4.
+ */
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface IConnectionPool {
+    // 获得连接
+    Connection getConnection();
+
+    // 获得当前连接
+    Connection getCurrentConnecton();
+
+    // 回收连接
+    void releaseConn(Connection conn) throws SQLException;
+
+    // 销毁清空
+    void destroy();
+
+    // 连接池是活动状态
+    boolean isActive();
+
+    // 定时器，检查连接池
+    void cheackPool();
+}
